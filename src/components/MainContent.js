e9a44478ec18e6e8e68572edf94027996c9f5b23
abhishek giri <akgiri@student.iul.ac.in>
2023-12-17 17:36:22 +0530

    added styles file

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,47 +1,20 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import Styles from "./styles/MainContentStyles";
 
 const MainContent = () => {
   return (
-    <div style={{ margin: 40 }}>
-      <div
-        style={{
-          fontWeight: 600,
-          fontStyle: "italic",
-          fontFamily: "cursive",
-          fontSize: 60,
-        }}
-      >
-        From Test Prep to Top Colleges!
-      </div>
+    <div style={Styles.mainContainer}>
+      <div style={Styles.subContainer}>From Test Prep to Top Colleges!</div>
       <div style={{ fontWeight: 500 }}>
         We at Crack Collage guide students through the SAT, ACT, AP, IB, IGCSE,
         and Pre-College Prep.
         <br /> Join us for in-person or online classes.
       </div>
-      <div
-        style={{
-          display: "flex",
-          margin: 20,
-          borderWidth: 2,
-          borderStyle: "solid",
-          borderColor: "black",
-          borderRadius: 20,
-          padding: 10,
-          width: "fit-content",
-        }}
-      >
+      <div style={Styles.phoneContainer}>
         <a href="tel:+91-9940306269">
-          <div
-            style={{
-              fontWeight: 600,
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            Call Us{" "}
-          </div>
+          <div style={Styles.callUs}>Call Us</div>
           <div style={{ display: "flex", flexDirection: "row" }}>
             <FontAwesomeIcon icon={faPhone} size="2x" />
             <div style={{ fontWeight: 700 }}>+91-9940306269</div>
---
+++ src/components/styles/MainContentStyles.js
@@ -0,0 +1,24 @@
+export default {
+  mainContainer: { margin: 40 },
+  subContainer: {
+    fontWeight: 600,
+    fontStyle: "italic",
+    fontFamily: "cursive",
+    fontSize: 60,
+  },
+  phoneContainer: {
+    display: "flex",
+    margin: 20,
+    borderWidth: 2,
+    borderStyle: "solid",
+    borderColor: "black",
+    borderRadius: 20,
+    padding: 10,
+    width: "fit-content",
+  },
+  callUs: {
+    fontWeight: 600,
+    display: "flex",
+    justifyContent: "center",
+  },
+};
